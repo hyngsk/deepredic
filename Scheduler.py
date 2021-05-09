22bb50f4cd7bf94fdd5fab6dbd654b2682f56248
HyeongSuk, Oh <hyngsk.o@gmail.com>
2021-05-09 14:31:27 +0900

    싱글톤 패턴

--- Scheduler.py
+++ Scheduler.py
@@ -6,24 +6,22 @@ from upbit import Upbit
 
 
 class Scheduler:
-    _instance = None
-
-    @classmethod
-    def _getInstance(cls):
-        return cls._instance
-
-    @classmethod
-    def instance(cls, *args, **kargs):
-        cls._instance = cls(*args, **kargs)
-        cls.instance = cls._getInstance
-        return cls._instance
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, "_instance"):         # Foo 클래스 객체에 _instance 속성이 없다면
+            print("__new__ is called\n")
+            cls._instance = super().__new__(cls)  # Foo 클래스의 객체를 생성하고 Foo._instance로 바인딩
+        return cls._instance                      # Foo._instance를 리턴
 
     def __init__(self):
+        cls = type(self)
+        if not hasattr(cls, "_init"):  # Foo 클래스 객체에 _init 속성이 없다면
+            print("__init__ is called\n")
         self.upbit = Upbit()
         self.sched = BackgroundScheduler()
         self.sched.start()
         self.job_id = ''
         data_setup()
+        cls._init = True
 
     def __del__(self):
         self.shutdown()
