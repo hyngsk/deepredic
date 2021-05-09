202a30a190231394837cd9223175d1cc34b7dc58
HyeongSuk, Oh <hyngsk.o@gmail.com>
2021-05-09 15:01:06 +0900

    싱글톤 패턴

--- app.py
+++ app.py
@@ -135,16 +135,18 @@ def root():
 
 
 if __name__ == '__main__':
+    pass
     # scheduler = Scheduler()
     # app.logger.info(scheduler.scheduler('cron', "Every1Hour"))
     # app.logger.info(scheduler.scheduler('cron', "Every15Minutes"))
-    app.debug = True
-    app.run(host='0.0.0.0', port=443, threaded=False)
+    # app.debug = True
+    # app.run(host='0.0.0.0', port=443, threaded=False)
 else:
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
-    a = Scheduler.scheduler('cron', "Every1Hour")
-    b = Scheduler.scheduler('cron', "Every15Minutes")
+    scheduler = Scheduler()
+    a = scheduler.scheduler('cron', "Every1Hour")
+    b = scheduler.scheduler('cron', "Every15Minutes")
     app.logger.info(a)
     app.logger.info(b)
