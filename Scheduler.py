c23850802795c18de805b652ba5bf597eca4d22b
HyeongSuk, Oh <hyngsk.o@gmail.com>
2021-05-09 14:07:32 +0900

    싱글톤 패턴

--- Scheduler.py
+++ Scheduler.py
@@ -1,4 +1,3 @@
-
 from apscheduler.jobstores.base import JobLookupError
 from apscheduler.schedulers.background import BackgroundScheduler
 
@@ -7,6 +6,18 @@ from upbit import Upbit
 
 
 class Scheduler:
+    _instance = None
+
+    @classmethod
+    def _getInstance(cls):
+        return cls._instance
+
+    @classmethod
+    def instance(cls, *args, **kargs):
+        cls._instance = cls(*args, **kargs)
+        cls.instance = cls._getInstance
+        return cls._instance
+
     def __init__(self):
         self.upbit = Upbit()
         self.sched = BackgroundScheduler()
