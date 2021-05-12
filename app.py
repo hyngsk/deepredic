c0dd35f5c90214a895c63d03f6e5b5f0789b2ffc
HyeongSuk, Oh <hyngsk.o@gmail.com>
2021-05-12 21:30:05 +0900

    로그 ELB-HealthChecker 제거 완료

--- app.py
+++ app.py
@@ -117,7 +117,6 @@ def root():
     if str(request.user_agent) != "ELB-HealthChecker/2.0":
         app.logger.info('\n' + '*' * 30 + f'\n{str(request.host)} {str(request.host_url)}\n' + '*' * 30)
         app.logger.info('\n' + '*' * 30 + f'\n{str(request.headers)}\n' + '*' * 30)
-
     market = request.args.get('market')
     app.logger.info(f'requested market : {market}')
     # print(str(request.user_agent))
